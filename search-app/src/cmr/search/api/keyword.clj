158c1a7982071cd8f32b2e5a54ea3f3ada29c118
Chris Durbin <christopher.d.durbin@nasa.gov>
2015-08-31 13:34:32 -0400

    CMR-1980: Move keyword endpoint to search application and get rid of endpoint for retrieving all types together in a single response

---
+++ search-app/src/cmr/search/api/keyword.clj
@@ -0,0 +1,40 @@
+(ns cmr.search.api.keyword
+  "Defines the HTTP URL routes for the keyword endpoint in the search application."
+  (:require [compojure.core :refer :all]
+            [cheshire.core :as json]
+            [cmr.common-app.services.kms-fetcher :as kf]
+            [cmr.transmit.kms :as kms]
+            [cmr.common.mime-types :as mt]
+            [cmr.common.services.errors :as errors]
+            [cmr.search.services.query-execution.facets-results-feature :as frf]))
+
+(defn- validate-keyword-scheme
+  "Throws a service error if the provided keyword-scheme is invalid."
+  [keyword-scheme]
+  (when-not (some? (keyword-scheme kms/keyword-scheme->field-names))
+    (errors/throw-service-error
+      :bad-request (format "The keyword scheme [%s] is not supported. Valid schemes are: %s"
+                           (name keyword-scheme)
+                           (pr-str (map name (keys kms/keyword-scheme->field-names)))))))
+
+; (defn- parse-hierarchical-keywords
+;   "Returns keywords in a hierarchical fashion for the given keyword scheme and keywords."
+;   [keyword-scheme keyword-hierarchy keywords]
+;   (when-let [subfield (first keyword-hierarchy)]
+;     (let [all-values-for-subfield (map subfield keywords)]
+;       (for [value all-values-for-subfield]
+;         (parse-hierarchical-keywords keyword-scheme (rest keyword-hierarchy) keywords)))))
+
+(def keyword-api-routes
+  (context "/keywords" []
+    ;; Return a list of keywords for the given scheme
+    (GET "/:keyword-scheme" {{:keys [keyword-scheme] :as params} :params
+                             request-context :request-context}
+      (let [keyword-scheme (keyword keyword-scheme)]
+        (validate-keyword-scheme keyword-scheme)
+        (let [keywords (vals (keyword-scheme (kf/get-gcmd-keywords-map request-context)))]
+          {:staus 200
+           :headers {"Content-Type" (mt/format->mime-type :json)}
+           :body (json/generate-string keywords)})))))
+
+
