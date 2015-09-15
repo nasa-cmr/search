2724166d39e158e3bee069b039d68c4a685bfb23
Jason Gilman <jason@element84.com>
2015-09-15 12:07:54 -0400

    CMR-1974: Adding initial implementation of collection search with tag parameters.

---
+++ search-app/src/cmr/search/services/tagging/tagging_service_messages.clj
@@ -0,0 +1,31 @@
+(ns cmr.search.services.tagging.tagging-service-messages
+  "This contains error response messages for the tagging service")
+
+(def token-required-for-tag-modification
+  "Tags cannot be modified without a valid user token.")
+
+(defn tag-already-exists
+  [tag concept-id]
+  (format "A tag with namespace [%s] and value [%s] already exists with concept id %s."
+          (:namespace tag) (:value tag) concept-id))
+
+(def field-may-not-contain-separator
+  "Validation format message so %s is included for the field"
+  "%s may not contain the Group Separator character. ASCII decimal value: 29 Unicode: U+001D")
+
+(defn cannot-change-field-value
+  [existing-value new-value]
+  (format "Tag %%s cannot be modified. Attempted to change existing value [%s] to [%s]"
+          existing-value new-value))
+
+(defn tag-does-not-exist
+  [concept-id]
+  (format "Tag could not be found with concept id [%s]" concept-id))
+
+(defn bad-tag-concept-id
+  [concept-id]
+  (format "[%s] is not a valid tag concept id." concept-id))
+
+(defn tag-deleted
+  [concept-id]
+  (format "Tag with concept id [%s] was deleted." concept-id))
